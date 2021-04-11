8c5f5d59a7a56054973e276cbbef7125a5bc1a3b
joao <joao.camargo@gmail.com>
2021-04-11 19:26:20 -0300

    project 19 finished

---
+++ Project-18/Project-18/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project-18
+//
+//  Created by joao camargo on 11/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        // Do any additional setup after loading the view.
+//        print("I'm inside the viewDidLoad() method!")
+//        print(1, 2, 3, 4, 5, separator: "-")
+//        print("Some message", terminator: "")
+//
+//
+//        assert(1 == 1, "Maths failure!")
+
+        for i in 1 ... 100 {
+            print("Got number \(i)")
+        }
+        
+    }
+
+
+}
+
---
+++ Project-19/Project-19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project-19
+//
+//  Created by joao camargo on 11/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
